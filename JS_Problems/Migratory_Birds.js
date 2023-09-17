1483dd1168e5170f96d5cb5b83a39f2d6e8a9170
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-09-17 20:16:45 +0530

    rearrange

--- JS_Problems/Migratory_Birds.js
+++ JS_Problems/Migratory_Birds.js
@@ -1,11 +1,16 @@
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
 function migratoryBirds(arr) {
-  let maxOccurence = 0;
-  let maxNumber = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == arr[i + 1]) {
-      maxOccurence += 1;
-    } else {
-      maxOccurence += 0;
+    let count = new Array(6).fill(0);
+    for (let bird of arr) {
+      count[bird]++;
     }
+    return count.indexOf(Math.max(...count));
   }
 }
