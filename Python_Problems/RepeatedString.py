f472dcb321db31d2dade55503012ce32318f5603
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-01-17 17:42:26 +0530

    Create RepeatedString.py

---
+++ Python_Problems/RepeatedString.py
@@ -0,0 +1,20 @@
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    count_a = s.count('a')
+    repeat_full = n//len(s)
+    remaining_s = n % len(s)
+    repeat_partial = s[:remaining_s].count('a')
+    total_count = (count_a*repeat_full)+(remaining_s*remaining_s)
+
+    return total_count
+
+
+print(repeatedString(s='aba', n=10))
