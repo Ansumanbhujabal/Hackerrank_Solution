d843a994fe1eb7029f5ac0408404b415ad1481af
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-02-19 22:48:47 +0530

    Create taumbday.py

---
+++ Python_Problems/taumbday.py
@@ -0,0 +1,8 @@
+"""
+Taum is planning to celebrate the birthday of his friend, Diksha. There are two types of gifts that Diksha wants from Taum: one is black and the other is white. To make her happy, Taum has to buy  black gifts and  white gifts.
+
+The cost of each black gift is  units.
+The cost of every white gift is  units.
+The cost to convert a black gift into white gift or vice versa is  units.
+Determine the minimum cost of Diksha's gifts.
+"""
