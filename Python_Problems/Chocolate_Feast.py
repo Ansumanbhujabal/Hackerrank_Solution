1bb751cb21e78f8efe301d551e9c7814e0ffa1d3
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-02-20 23:30:06 +0530

    Create Chocolate_Feast.py

---
+++ Python_Problems/Chocolate_Feast.py
@@ -0,0 +1,8 @@
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
