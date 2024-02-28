a5b22d31bf031d3e913b9e11b8662708db5f292d
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-02-28 19:36:34 +0530

    Update beautifulTriplet.py

---
+++ Python_Problems/beautifulTriplet.py
@@ -0,0 +1,8 @@
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
