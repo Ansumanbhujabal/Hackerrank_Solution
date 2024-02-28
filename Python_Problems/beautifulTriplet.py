27b21c69a58413dfe5ef296629bcd9f6db30cd08
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-02-28 19:37:29 +0530

    Update beautifulTriplet.py

--- Python_Problems/beautifulTriplet.py
+++ Python_Problems/beautifulTriplet.py
@@ -6,3 +6,17 @@
 #  1. INTEGER d
 #  2. INTEGER_ARRAY arr
 #
+def beautifulTriplets(d, arr):
+    total = []
+    for i in arr:
+        j = i+d
+        if j in arr:
+            k = j+d
+            if k in arr:
+                total.append(i)
+                total.append(j)
+                total.append(k)
+    return len(total)//3
+
+
+print(beautifulTriplets(1, [2, 2, 3, 4, 5]))
