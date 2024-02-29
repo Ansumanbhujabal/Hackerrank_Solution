d78ab000d8cfa4fdcc8b88088f8f974123d6f770
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-02-29 17:26:00 +0530

    Update halloweenSale.py

--- Python_Problems/halloweenSale.py
+++ Python_Problems/halloweenSale.py
@@ -8,3 +8,11 @@
 #  3. INTEGER m
 #  4. INTEGER s
 #
+def howManyGames(p, d, m, s):
+    count = 0
+    while not s < p:  # can mean s>p or s=p  same as s>=p
+        s -= p
+        new_price = p - d
+        p = new_price if new_price > m else m
+        count += 1
+    return count
