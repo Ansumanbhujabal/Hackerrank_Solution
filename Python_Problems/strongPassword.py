3654f5d8969842c3b807a59fe6cfdf7181975a4a
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-02-29 20:32:27 +0530

    Create strongPassword.py

---
+++ Python_Problems/strongPassword.py
@@ -0,0 +1,6 @@
+# Complete the 'minimumNumber' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. STRING password
