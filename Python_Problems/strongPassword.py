e4e0fd921db925607522217ecad5d9f4f0a6ca18
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-02-29 20:32:37 +0530

    Update strongPassword.py

--- Python_Problems/strongPassword.py
+++ Python_Problems/strongPassword.py
@@ -4,3 +4,16 @@
 # The function accepts following parameters:
 #  1. INTEGER n
 #  2. STRING password
+def minimumNumber(n, password):
+    upper = digits = lower = special = 0
+    for i in password:
+        if i.isupper():
+            upper += 1
+        elif i.isdigit():
+            digits += 1
+        elif i.islower():
+            lower += 1
+        elif i in "!@#$%^&*()-+":
+            special += 1
+    ls = [upper, digits, lower, special]
+    return max(ls.count(0), 6 - n)
